0d3315efe53d2097a290635b46a7e7580b437119
ParagK19 <66519286+ParagK19@users.noreply.github.com>
2024-08-11 23:53:07 +0530

    Add files via upload

---
+++ about.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title></title>
+    <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@100;200;400&display=swap" rel="stylesheet">
+
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">ParagsWeb</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav ml-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Services</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="about.php">About</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Contact</a>
+        </li>
+     
+      </ul>
+      <form class="d-flex" role="search">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
+
+<div class="jumbotron">
+  <h1>ParagSWeb</h1>
+  <p>Coding is Life</p>
+</div>
+
+<section class="my-5">
+   <div class="py-5">
+   <h2 class="text-center">About Us</h2>
+   </div>
+<div class="container-fluid">
+<div class="row">
+  <div class="col-lg-6 col-md-6 col-12">
+    <img src="images/band4.jpg" class="img-fluid aboutimg">
+    </div>
+    <div class="col-lg-6 col-md-6 col-12">
+    <h2 class="display-4">I am Parag Kamble</h2>
+    <p class="py-3">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugit cum sit, enim unde velit atque nobis? Iste quae, rem amet quasi voluptates tempore autem quidem tenetur voluptatem ut mollitia cupiditate!</p>
+    <a href="about.php" class="btn btn-success">Check More</a>
+    </div>
+</div>
+</div>
+</section>
+
+  <footer>
+    <p class="p-3 bg-dark text-white text-center">@paragk181@gmail</p>
+    </footer>
+
+</body>
+</html>
